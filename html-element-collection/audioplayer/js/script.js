c6e76bb9735fc0b93edb90a3f806fa952f8b34ae
Mike <mihail-orlenko@mail.ru>
2018-07-30 13:52:01 +0300

    audioplayer v.1

---
+++ html-element-collection/audioplayer/js/script.js
@@ -0,0 +1,67 @@
+'use strict';
+
+const audioList = [
+  'LA Chill Tour',
+  'This is it band',
+  'LA Fusion Jam'
+];
+
+const playerWrap = document.getElementsByClassName('mediaplayer')[0];
+const player = playerWrap.getElementsByTagName('audio')[0];
+const controls = playerWrap.getElementsByTagName('button');
+const songTitle = playerWrap.getElementsByClassName('title')[0];
+let songNum = 0;
+
+player.src = changeSrc(audioList[songNum]);
+
+function changeSrc(songName) {
+  return 'mp3/' + songName + '.mp3';
+}
+
+function playstate() {
+  if (!playerWrap.classList.contains('play')) {
+    player.play();
+  } else {
+    player.pause();
+  }
+
+  playerWrap.classList.toggle('play');
+}
+
+function stop() {
+  if (player.currentTime > 0) {
+    player.pause();
+    player.currentTime = 0;
+    playerWrap.classList.remove('play');
+  }
+}
+
+function changeSong() {
+  if (this.className === 'back') {
+    songNum--;
+
+    if (songNum === -1) {
+      songNum = audioList.length - 1;
+    }
+  } else {
+    songNum++;
+
+    if (songNum > (audioList.length - 1)) {
+      songNum = 0;
+    }
+  }
+
+  player.src = changeSrc(audioList[songNum]);
+  songTitle.title = audioList[songNum];
+  playerWrap.classList.remove('play');
+}
+
+for (let btn of controls) {
+  if (btn.className === 'playstate') {
+    btn.onclick = playstate;
+  } else if (btn.className === 'stop') {
+    btn.onclick = stop;
+  } else if (btn.className === 'back' || btn.className === 'next') {
+    btn.onclick = changeSong;
+  }
+}
