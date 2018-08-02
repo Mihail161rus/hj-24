ae3b98dadc7519c55ca69bd90c35cf6e6dfb23e7
Mihail161rus <mihail-orlenko@mail.ru>
2018-08-02 23:54:15 +0300

    решение пасхалка v.2

--- event-object/easter-egg/js/script.js
+++ event-object/easter-egg/js/script.js
@@ -1,8 +1,5 @@
 'use strict';
 
-const keysEntered = [];
-const secretKey = 'ytnjkjubz';
-
 function keysHandler(event) {
   if (event.ctrlKey && event.altKey && event.code === 'KeyT') {
     const navMenu = document.getElementsByTagName('nav')[0];
@@ -10,14 +7,19 @@ function keysHandler(event) {
     navMenu.classList.toggle('visible');
   }
 
-  let keyEntered = event.code.replace('Key', '').toLowerCase();
-  keysEntered.push(keyEntered);
+  if (typeof event.code === 'string') {
+    let keyEntered = event.code.replace('Key', '').toLowerCase();
+    keysEntered.push(keyEntered);
+  }
 
   if (keysEntered.length === 9 && keysEntered.join('') === secretKey) {
     document.getElementsByClassName('secret')[0].classList.add('visible');
-  } else {
+  } else if (keysEntered.length >= 9) {
     keysEntered.splice(0, keysEntered.length)
   }
 }
+
+const keysEntered = [];
+const secretKey = 'ytnjkjubz';
 
 document.addEventListener('keydown', keysHandler);
