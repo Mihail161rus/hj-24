18e502c05ba2683c6465518cd76cdf6466f5bf18
Mihail161rus <mihail-orlenko@mail.ru>
2018-08-02 23:59:36 +0300

    dropdown v.3

--- html-element-collection/dropdown-menu/js/script.js
+++ html-element-collection/dropdown-menu/js/script.js
@@ -1,15 +1,9 @@
 'use strict';
 
 const btnDropDown = document.getElementsByClassName('wrapper-dropdown')[0];
-const dropdown = document.getElementsByClassName('dropdown')[0];
-const items = dropdown.getElementsByTagName('li');
 
 function toggleDropDown() {
   this.classList.toggle('active');
 }
 
 btnDropDown.onclick = toggleDropDown;
-
-for (let item of items) {
-  item.onclick = toggleDropDown;
-}
