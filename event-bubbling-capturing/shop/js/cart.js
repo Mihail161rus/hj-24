943e26a2be5b5a8cd08a5ae87908fcb6c9384b15
Mike <mihail-orlenko@mail.ru>
2018-08-15 12:30:23 +0300

    решение задания v.1

--- event-bubbling-capturing/shop/js/cart.js
+++ event-bubbling-capturing/shop/js/cart.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const itemsContainer = document.querySelector('.items-list');
+itemsContainer.addEventListener('click', clickAddToCart);
+
+function clickAddToCart(event) {
+  if (event.target.classList.contains('add-to-cart')) {
+    event.preventDefault();
+    
+    let item = {
+      title: event.target.dataset.title,
+      price: event.target.dataset.price
+    };
+
+    addToCart(item);
+  }
+}
