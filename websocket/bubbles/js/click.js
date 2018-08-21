8a439b6b825b9d82c8f97e15ec7072c483e4eb1a
Mike <mihail-orlenko@mail.ru>
2018-08-21 11:09:50 +0300

    решение задания v.1

--- websocket/bubbles/js/click.js
+++ websocket/bubbles/js/click.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const connection = new WebSocket('wss://neto-api.herokuapp.com/mouse');
+
+showBubbles(connection);
+
+document.addEventListener('click', (event) => {
+  connection.send(JSON.stringify({
+    x: event.clientX, y: event.clientY
+    })
+  );
+});
+
+window.addEventListener('beforeunload', () => {
+  connection.close(1000, 'Пользователь завершил работу со страницей')
+});
