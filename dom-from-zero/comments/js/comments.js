64e0265d7800e8f303cd6adb1a7c19da919ac93f
Mike <mihail-orlenko@mail.ru>
2018-08-30 11:20:34 +0300

    решение задания v.1

--- dom-from-zero/comments/js/comments.js
+++ dom-from-zero/comments/js/comments.js
@@ -2,12 +2,55 @@
 
 function showComments(list) {
   const commentsContainer = document.querySelector('.comments');
-  const comments = list.map(createComment).join('');
-  commentsContainer.innerHTML += comments;
+  const comments = list.map(createComment);
+
+  comments.forEach(comment => commentsContainer.appendChild(comment));
+}
+
+function el(tagName, attributes, children) {
+  const element = document.createElement(tagName);
+
+  if (typeof attributes === 'object') {
+    Object.keys(attributes).forEach(i => element.setAttribute(i, attributes[i]));
+  }
+
+  if (typeof children === 'string') {
+    element.textContent = children;
+  } else if (children instanceof Array) {
+    children.forEach(child => element.appendChild(child));
+  }
+
+  return element;
 }
 
 function createComment(comment) {
-  return `<div class="comment-wrap">
+  const textList = comment.text.split('\n');
+
+  return el('div', {class: 'comment-wrap'}, [
+    el('div', {class: 'photo', title: comment.author.name}, [
+      el('div', {class: 'avatar', style: `background-image: url('${comment.author.pic}')`})
+    ]),
+    el('div', {class: 'comment-block'}, [
+      el('p', {class: 'comment-text'}, textList.map(item => {
+        const br = el('br');
+        const span = el('span');
+
+        span.textContent = item;
+        span.appendChild(br);
+
+        return span;
+      })),
+      el('div', {class: 'bottom-comment'}, [
+        el('div', {class: 'comment-date'}, new Date(comment.date).toLocaleString('ru-Ru')),
+        el('ul', {class: 'comment-actions'}, [
+          el('li', {class: 'complain'}, 'Пожаловаться'),
+          el('li', {class: 'reply'}, 'Ответить')
+        ])
+      ])
+    ])
+  ]);
+
+  /*return `<div class="comment-wrap">
     <div class="photo" title="${comment.author.name}">
       <div class="avatar" style="background-image: url('${comment.author.pic}')"></div>
     </div>
@@ -23,7 +66,7 @@ function createComment(comment) {
         </ul>
       </div>
     </div>
-  </div>`
+  </div>`*/
 }
 
 fetch('https://neto-api.herokuapp.com/comments')
