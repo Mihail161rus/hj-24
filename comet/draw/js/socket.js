0acccccb62aa1b157388171da12493c6aba1514a
Mike <mihail-orlenko@mail.ru>
2018-09-02 12:30:10 +0300

    решение задания v.1

--- comet/draw/js/socket.js
+++ comet/draw/js/socket.js
@@ -1,1 +1,7 @@
 'use strict';
+
+const websocket = new WebSocket('wss://neto-api.herokuapp.com/draw');
+
+window.editor.addEventListener('update', event => {
+  event.canvas.toBlob(blob => websocket.send(blob));
+});
